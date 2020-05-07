d746d548d6444b077e93ec2e8b3cd54e0cd5bd92
Fernando Jorquera <fernando.jorquera@mavericks.cool>
2020-05-07 17:48:21 -0400

    Initial Commit

---
+++ NewsSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NewsSwiftUI
+//
+//  Created by Mavericks's iOS Dev on 07-05-20.
+//  Copyright © 2020 Mavericks. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
